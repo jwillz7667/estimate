b1200fde986060e08c72432e314d378cbc0d6ac7
jwillz7667 <jwillz7667@gmail.com>
2025-12-06 02:51:35 -0600

    Initial Commit

---
+++ ProjectEstimateTests/ProjectEstimateTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectEstimateTests.swift
+//  ProjectEstimateTests
+//
+//  Created by Justin Williams on 12/6/25.
+//
+
+import Testing
+@testable import ProjectEstimate
+
+struct ProjectEstimateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
